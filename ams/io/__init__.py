ad067a0073faa017848f67d73704ccc5fc46ebbb
jinningwang <jinningwang@users.noreply.github.com>
2023-02-16 16:53:54 -0500

    Simplify parser

--- ams/io/__init__.py
+++ ams/io/__init__.py
@@ -2,10 +2,4 @@
 AMS input parsers and output formatters.
 """
 
-import io
-from ams.io import xlsx   # NOQA
-
-def parse(system):
-    """Parse input files"""
-    pass
-    return True
+from andes.io import parse # NOQA
---
+++ ams/io/matpower.py
@@ -0,0 +1,8 @@
+"""
+MATPOWER parser revised from ANDES matpower parser.
+"""
+
+from andes.io.matpower import (testlines, read, m2mpc, mpc2system,
+                               _get_bus_id_caller, system2mpc)  # NOQA
+
+# TODO: MATPOWER parser needs to be revised later on
