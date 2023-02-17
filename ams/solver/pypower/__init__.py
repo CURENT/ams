0e471af01d2177389871b1e7e5b4125b45223f68
jinningwang <jinningwang@users.noreply.github.com>
2023-02-17 12:37:02 -0500

    Add pp_system to hold PyPower case

--- ams/solver/pypower/__init__.py
+++ ams/solver/pypower/__init__.py
@@ -9,3 +9,40 @@ This module is developed from module pandapower.pypower.
 """
 
 # TODO: add a comparasion function of this module with pandapower.pypower
+from numpy import array
+
+
+class pp_system():
+    """
+    PyPower system class, as the interface to PyPower
+    """
+
+    def __init__(self, case):
+        """
+        PyPower system class, as the interface to PyPower.
+
+        Parameters
+        ----------
+        case: str
+            PyPower case file name, such as 'case14.py'
+
+        Examples
+        --------
+        >>> import ams
+        >>> spp = pp_system(case=ams.get_case('pypower/case14.py'))
+        """
+        exec(open(f"{case}").read())
+        func_name = case.split('/')[-1].rstrip('.py')
+        self.ppc = eval(f"{func_name}()")  # the dict of PyPower case
+
+    def set_case(self, case):
+        pass
+
+    def set_solver(self, solver):
+        pass
+
+    def solve(self):
+        pass
+
+    def get_results(self):
+        pass
