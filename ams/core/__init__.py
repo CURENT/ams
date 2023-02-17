cc34a95acc7c779f508b1b486839f3fe73929a35
jinningwang <jinningwang@users.noreply.github.com>
2023-02-17 08:44:06 -0500

    Simplify modules ams.core.model and ams.param

--- ams/core/__init__.py
+++ ams/core/__init__.py
@@ -1,2 +1,2 @@
+import ams.core.model # NOQA
 from ams.core.param import (BaseParam, ExtParam, IdxParam, NumParam, ExtParam)  # NOQA
-from ams.core.model import ModelData  # NOQA
--- ams/core/model/__init__.py
+++ ams/core/model/__init__.py
@@ -1,1 +1,10 @@
-from .modeldata import ModelData  # NOQA
+"""
+Module for model definition and model data definition.
+
+This module is revised from andes.core.model.
+"""
+
+from andes.core.model import Model  # NOQA
+from andes.core.model import ModelData
+
+# TODO: some definitions might need to be revised or blocked in the future
