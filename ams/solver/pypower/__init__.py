7d025904e8e26588c1291be67d2b1626acc39de8
jinningwang <jinningwang@users.noreply.github.com>
2023-02-17 09:23:38 -0500

    Init module pypower from module pandapower.pypower

---
+++ ams/solver/pypower/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) 1996-2015 PSERC. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+"""
+PYPOWER solves power flow and Optimal Power Flow (OPF) problems.
+
+This module is developed from module pandapower.pypower.
+"""
+
+# TODO: add a comparasion function of this module with pandapower.pypower
