539f3bb1604eee760157193176e459f49e353b8b
jinningwang <jinningwang@users.noreply.github.com>
2023-02-17 13:02:36 -0500

    Remove pypower files

--- ams/solver/pypower/__init__.py
+++ ams/solver/pypower/__init__.py
@@ -5,7 +5,7 @@
 """
 PYPOWER solves power flow and Optimal Power Flow (OPF) problems.
 
-This module is developed from module pandapower.pypower.
+This module is developed from PyPower maintained under pandapower.
 """
 
 # TODO: add a comparasion function of this module with pandapower.pypower
@@ -27,11 +27,17 @@ class pp_system():
 
     def __init__(self):
         self.ppc = None
+        # 'ppc' for PyPower case, 'mpc' for Matpower case
+        # 'ads' for ANDES system, 'ams' for AMS system
+        # TODO: this source_type seems not necessary
+        self.source_type = None
 
     def from_ppc(self, case):
         exec(open(f"{case}").read())
+        # NOTE: the following line is not robust
         func_name = case.split('/')[-1].rstrip('.py')
         self.ppc = eval(f"{func_name}()")
+        self.source_type = 'ppc'
 
     def set_case(self, case):
         pass
