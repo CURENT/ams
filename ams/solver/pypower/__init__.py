037e680a775578a470c93051e88777c276381689
jinningwang <jinningwang@users.noreply.github.com>
2023-02-17 14:54:50 -0500

    Add some extra pypower files

--- ams/solver/pypower/__init__.py
+++ ams/solver/pypower/__init__.py
@@ -3,8 +3,3 @@ PYPOWER solves power flow and Optimal Power Flow (OPF) problems.
 
 This module is developed from PyPower.
 """
-
-# TODO: add a comparasion function of this module with pandapower.pypower
-from numpy import array
-
-from ams.solver.pypower.interface import ppi
