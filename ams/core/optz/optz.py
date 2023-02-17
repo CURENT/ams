9c8be3a55143abd7e003239f34080d74ac550339
jinningwang <jinningwang@users.noreply.github.com>
2023-02-17 09:10:19 -0500

    Revise ams.core.optz.Var

--- ams/core/optz/optz.py
+++ ams/core/optz/optz.py
@@ -6,22 +6,19 @@ This module is developed from andes.core.var.
 
 from andes.core.var import BaseVar
 
-class BaseOptz():
-    """
-    The base optimization class.
-    """
 
-    def __init__(self):
-        pass
-
-
-class Var(BaseOptz):
+class Var(BaseVar):
     """
-    The variable class.
+    The variable class for dispatch modeling.
+
+    This class connects the variables in AMS with those in the solver.
+
+    This class is revised from andes.core.var.BaseVar.
     """
 
     def __init__(self):
-        BaseOptz.__init(self)
+        BaseVar.__init(self)
+        # TODO: the definition might need to be revised: 1) block unused methods; 2) add new attributes
 
 
 class Obj(BaseOptz):
