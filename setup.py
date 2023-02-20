0019522806a1a5e03a9bcbf4dd8315415265e141
jinningwang <jinningwang@users.noreply.github.com>
2023-02-20 16:14:27 -0500

    Fix setup config

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ setup(
     author="Hantao Cui",
     author_email='cuihantao@gmail.com',
     url='https://github.com/jinningwang/ams',
-    packages=find_packages(include=[pandapower.pypower], exclude=[]),
+    packages=find_packages(include=[], exclude=[]),
     entry_points={
         'console_scripts': [
             'ams = ams.cli:main',
