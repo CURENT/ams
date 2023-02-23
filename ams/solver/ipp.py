805957970c8006cae12b5e6d9735fa1278bb0be2
jinningwang <jinningwang@users.noreply.github.com>
2023-02-23 16:47:00 -0500

    Add rest bus data conversion into to_ppc

--- ams/solver/ipp.py
+++ ams/solver/ipp.py
@@ -57,7 +57,7 @@ def to_ppc(ssp) -> dict:
     # system MVA base
     ppc["baseMVA"] = ssp.config.mva
 
-    # bus data
+    ## initialize bus data
     ssp_bus = ssp.Bus.as_df().rename(columns={'idx': 'bus'})
 
     # NOTE: bus data: bus_i type Pd Qd Gs Bs area Vm Va baseKV zone Vmax Vmin
@@ -69,7 +69,7 @@ def to_ppc(ssp) -> dict:
     ppc_bus['bus_i'] = ssp_bus['bus']  # TODO: will run into error if idx type is string
     ppc_bus['type'] = 4
 
-    # load data
+    ## load data
     ssp_pq = ssp.PQ.as_df()
     ssp_pq[['p0', 'q0']] = ssp_pq[['p0', 'q0']] * ssp.config.mva
     ssp_pq['type'] = 1
@@ -79,7 +79,7 @@ def to_ppc(ssp) -> dict:
     ppc_bus['Pd'] = ppc_load['Pd']
     ppc_bus['Qd'] = ppc_load['Qd']
 
-    # shunt data
+    ## shunt data
     ssp_shunt = ssp.Shunt.as_df()
     ssp_shunt['g'] = ssp_shunt['g'] * ssp_shunt['u']
     ssp_shunt['b'] = ssp_shunt['b'] * ssp_shunt['u']
@@ -90,13 +90,24 @@ def to_ppc(ssp) -> dict:
     ppc_bus['Gs'] = ppc_y['Gs']
     ppc_bus['Bs'] = ppc_y['Bs']
 
-    ppc_bus['area'] = 1
+    ## rest of the bus data
+    ppc_bus['area'] = ssp_bus['area']
     ppc_bus['Vm'] = ssp_bus['v0']
     ppc_bus['Va'] = ssp_bus['a0']
     ppc_bus['baseKV'] = ssp_bus['Vn']
     ppc_bus['zone'] = ssp_bus['owner']
     ppc_bus['Vmax'] = ssp_bus['vmax']
     ppc_bus['Vmin'] = ssp_bus['vmin']
+
+    ## generator
+
+    ## branch
+
+    ## areas
+
+    ## gencost
+
+    ## output bus data
     ppc["bus"] = ppc_bus.values
 
     return ppc
