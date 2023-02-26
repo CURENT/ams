a0135ab829a93a90209c04470ef9a4814afda634
jinningwang <jinninggm@gmail.com>
2023-02-25 23:28:58 -0500

    Fix makebus

--- ams/solver/pypower/make/makebus.py
+++ ams/solver/pypower/make/makebus.py
@@ -6,8 +6,7 @@ import logging
 from numpy import ones, flatnonzero as find
 from scipy.sparse import csr_matrix as sparse
 
-from ams.solver.pypower.idx_bus import PD, QD
-from ams.solver.pypower.idx_gen import GEN_BUS, PG, QG, GEN_STATUS
+import ams.solver.pypower.idx.constants as const
 
 logger = logging.getLogger(__name__)
 
@@ -30,8 +29,8 @@ def makeSbus(baseMVA, bus, gen):
         Complex bus power injections.
     """
     ## generator info
-    on = find(gen[:, GEN_STATUS] > 0)      ## which generators are on?
-    gbus = gen[on, GEN_BUS]                   ## what buses are they at?
+    on = find(gen[:, const.gen['GEN_STATUS']] > 0)      ## which generators are on?
+    gbus = gen[on, const.gen['GEN_BUS']]                   ## what buses are they at?
 
     ## form net complex bus power injection vector
     nb = bus.shape[0]
@@ -40,8 +39,8 @@ def makeSbus(baseMVA, bus, gen):
     Cg = sparse((ones(ngon), (gbus, range(ngon))), (nb, ngon))
 
     ## power injected by gens plus power injected by loads converted to p.u.
-    Sbus = ( Cg * (gen[on, PG] + 1j * gen[on, QG]) -
-             (bus[:, PD] + 1j * bus[:, QD]) ) / baseMVA
+    Sbus = ( Cg * (gen[on, const.gen['PG']] + 1j * gen[on, const.gen['QG']]) -
+             (bus[:, const.bus['PD']] + 1j * bus[:, const.bus['QD']]) ) / baseMVA
 
     return Sbus
 
@@ -66,8 +65,8 @@ def makeSbus(baseMVA, bus, gen):
         Complex bus power injections.
     """
     ## generator info
-    on = find(gen[:, GEN_STATUS] > 0)      ## which generators are on?
-    gbus = gen[on, GEN_BUS]                   ## what buses are they at?
+    on = find(gen[:, const.gen['GEN_STATUS']] > 0)      ## which generators are on?
+    gbus = gen[on, const.gen['GEN_BUS']]                   ## what buses are they at?
 
     ## form net complex bus power injection vector
     nb = bus.shape[0]
@@ -76,8 +75,8 @@ def makeSbus(baseMVA, bus, gen):
     Cg = sparse((ones(ngon), (gbus, range(ngon))), (nb, ngon))
 
     ## power injected by gens plus power injected by loads converted to p.u.
-    Sbus = ( Cg * (gen[on, PG] + 1j * gen[on, QG]) -
-             (bus[:, PD] + 1j * bus[:, QD]) ) / baseMVA
+    Sbus = ( Cg * (gen[on, const.gen['PG']] + 1j * gen[on, const.gen['QG']]) -
+             (bus[:, const.bus['PD']] + 1j * bus[:, const.bus['QD']]) ) / baseMVA
 
     return Sbus
 
