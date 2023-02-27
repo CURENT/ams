c0b92c2364255c1668cdbe0a273a28a208e808b4
jinningwang <jinningwang@users.noreply.github.com>
2023-02-27 13:08:56 -0500

    Add Algeb for device models

---
+++ ams/core/var.py
@@ -0,0 +1,29 @@
+"""
+Base class for variables.
+"""
+
+from typing import Optional
+from andes.shared import np
+
+
+class Algeb:
+    """
+    Algebraic variable class.
+    """
+
+    def __init__(self,
+                 name: Optional[str] = None,
+                 tex_name: Optional[str] = None,
+                 info: Optional[str] = None,
+                 unit: Optional[str] = None,
+                 ):
+        self.name = name
+        self.info = info
+        self.unit = unit
+
+        self.tex_name = tex_name if tex_name else name
+        self.owner = None  # instance of the owner Model
+        self.id = None     # variable internal index inside a model (assigned in run time)
+
+        self.a: np.ndarray = np.array([], dtype=int)
+        self.v: np.ndarray = np.array([], dtype=float)
