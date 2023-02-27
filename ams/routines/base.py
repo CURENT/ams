5de356ad63ed5f097ee82527b1330d4c21fd960c
jinningwang <jinningwang@users.noreply.github.com>
2023-02-27 13:08:32 -0500

    [WIP] Add routines

--- ams/routines/base.py
+++ ams/routines/base.py
@@ -2,13 +2,55 @@
 Base module for routines.
 """
 
-class BaseRT():
-    """Base routine."""
-    pass
+import logging # NOQA
+from collections import OrderedDict  # NOQA
+from andes.routines.base import BaseRoutine as andes_BaseRoutine  # NOQA
 
-    def __init__(self) -> None:
-        self.name = 'BaseRT'
+logger = logging.getLogger(__name__)
 
-    def run(self) -> None:
-        """Run the routine"""
+
+class BaseTable:
+    """
+    Base class for holding dispatch results of a device.
+    """
+    def __init__(self, columns=None):
+        self.columns = columns
+        self.data = OrderedDict()
+    
+    def as_df(self):
+        """
+        Convert the data to a pandas DataFrame.
+        """
+        pass
+
+
+class BaseResults:
+    """
+    Base class for holding dispatch results.
+    """
+    def __init__(self):
+        pass
+
+    def as_df(self):
+        """
+        Convert the data to a pandas DataFrame.
+        """
+        pass
+
+
+class BaseRoutine(andes_BaseRoutine):
+    """
+    Base routine class.
+    """
+    def __init__(self, system=None, config=None):
+        # TODO: may need revision on the __init__()
+        super().__init__()
+        self.name = 'BaseRoutine'
+        # TODO: how to organize the results?
+        self.res = BaseResults()
+
+    def run(self, **kwargs):
+        """
+        Run the routine.
+        """
         pass
---
+++ ams/routines/pflow.py
@@ -0,0 +1,4 @@
+"""
+Module for power flow calculation.
+"""
+
