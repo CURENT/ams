398df02715f4395c57ec2cbc869027ef71adb96f
jinningwang <jinningwang@users.noreply.github.com>
2023-03-03 16:36:47 -0500

    [WIP] PYPOWER parser

---
+++ ams/io/pypower.py
@@ -0,0 +1,43 @@
+"""
+PYPOWER reader for AMS.
+"""
+
+
+from ams.solver.ipp import load_ppc
+
+def testlines(infile):
+    """
+    Test if this file is in the PYPOWER format.
+
+    NOT YET IMPLEMENTED.
+    """
+    return True
+
+def read(system, file):
+    """
+    Read a PYPOWER data file into ppc, and build andes device elements.
+
+    TODO: add support for PYPOWER file in future versions.
+    """
+    ppc = load_ppc(file)
+    return ppc2system(ppc, system)
+
+
+def ppc2system(ppc: dict, system) -> bool:
+    """
+    Load an PYPOWER case dict into an empth AMS system.
+
+    Parameters
+    ----------
+    ppc : dict
+        The PYPOWER case dict.
+    system : ams.system
+        Empty AMS system to load data into.
+    
+    Returns
+    -------
+    bool
+        True if successful; False otherwise.
+    """
+    raise NotImplementedError('PYPOWER reader is not yet implemented.')
+    return True
