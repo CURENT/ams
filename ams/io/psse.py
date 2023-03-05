926e002e7bda52b830f56950b7a37c6d4f2b5b98
jinningwang <jinninggm@gmail.com>
2023-03-04 23:50:12 -0500

    Fix IO for psse and xlsx

--- ams/io/psse.py
+++ ams/io/psse.py
@@ -4,4 +4,4 @@ Excel reader and writer for AMS.
 This module leverages the existing parser and writer in andes.io.psse.
 """
 
-from andes.io.psse import read  # NOQA
+from andes.io.psse import (read, testlines)  # NOQA
