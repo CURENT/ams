d2bf8bf74b7728f7daa3ea1ba0571a5387d62438
jinningwang <jinningwang@users.noreply.github.com>
2023-05-12 15:13:19 -0400

    Refactor StaticGen

--- ams/models/static.py
+++ ams/models/static.py
@@ -57,7 +57,7 @@ class PQ(PQData, Model):
                               )
 
 
-class GENADD:
+class GenParam:
     """
     Additional parameters for static generators.
 
@@ -110,17 +110,13 @@ class GENADD:
                             tex_name=r'apf')
 
 
-class PV(PVData, GENADD, Model):
+class PVModel(Model):
     """
-    PV generator model.
-
-    TODO: implement type conversion in config
+    PV generator model (power flow) with q limit and PV-PQ conversion.
     """
 
-    def __init__(self, system, config):
-        PVData.__init__(self)
-        GENADD.__init__(self)
-        Model.__init__(self, system, config)
+    def __init__(self, system=None, config=None):
+        super().__init__(system, config)
         self.group = 'StaticGen'
 
         self.config.add(OrderedDict((('pv2pq', 0),
@@ -151,6 +147,11 @@ class PV(PVData, GENADD, Model):
                               err_tol=r"\epsilon_{tol}"
                               )
 
+        self.ud = Algeb(name='ud',
+                        tex_name=r'u_d',
+                        info='connection status decision',
+                        unit='bool',
+                        )
         self.p = Algeb(info='actual active power generation',
                        unit='p.u.',
                        tex_name='p',
@@ -163,36 +164,25 @@ class PV(PVData, GENADD, Model):
                        )
 
 
-class Slack(SlackData, GENADD, Model):
+class PV(PVData, GenParam, PVModel):
+    """
+    PV generator model.
+
+    TODO: implement type conversion in config
+    """
+
+    def __init__(self, system, config):
+        PVData.__init__(self)
+        GenParam.__init__(self)
+        PVModel.__init__(self, system, config)
+
+
+class Slack(SlackData, GenParam, PVModel):
     """
     Slack generator model.
     """
 
     def __init__(self, system=None, config=None):
         SlackData.__init__(self)
-        GENADD.__init__(self)
-        Model.__init__(self, system, config)
-        self.group = 'StaticGen'
-
-        self.p = Algeb(info='actual active power generation',
-                       unit='p.u.',
-                       tex_name='p',
-                       name='p',
-                       )
-        self.q = Algeb(info='actual reactive power generation',
-                       unit='p.u.',
-                       tex_name='q',
-                       name='q',
-                       )
-
-        self.config.add(OrderedDict((('av2pv', 0),
-                                     )))
-        self.config.add_extra("_help",
-                              av2pv="convert Slack to PV in PFlow at P limits",
-                              )
-        self.config.add_extra("_alt",
-                              av2pv=(0, 1),
-                              )
-        self.config.add_extra("_tex",
-                              av2pv="z_{av2pv}",
-                              )
+        GenParam.__init__(self)
+        PVModel.__init__(self, system, config)
