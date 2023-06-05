a2cb3693d812b44f5512b7f9df982ec4e648551c
jinningwang <jinningwang@users.noreply.github.com>
2023-06-05 16:22:28 -0400

    Draft interop

---
+++ ams/interop/andes.py
@@ -0,0 +1,14 @@
+"""
+Interface with ANDES
+"""
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def to_andes():
+    """
+    Convert the current model to ANDES format
+    """
+    pass
