83b5fc1fa7a6b49ace3dff3bc33e132b7fe102cf
jinningwang <jinningwang@users.noreply.github.com>
2023-06-06 14:02:24 -0400

    Refactor to_andes

--- ams/interop/andes.py
+++ ams/interop/andes.py
@@ -10,8 +10,9 @@ from andes.models import file_classes
 from andes.system import System as andes_system
 from andes.shared import pd, np
 from andes.utils.misc import elapsed
+from andes import load as andes_load
 
-from ams.io import input_formats, guess
+from ams.io import input_formats, guess, xlsx
 
 logger = logging.getLogger(__name__)
 
@@ -19,6 +20,7 @@ logger = logging.getLogger(__name__)
 def to_andes(system,
              setup=True,
              addfile=None,
+             overwrite=None,
              **kwargs):
     """
     Convert the current model to ANDES format.
@@ -33,65 +35,45 @@ def to_andes(system,
         Keyword arguments to be passed to `andes.system.System`.
     """
     t0, _ = elapsed()
+    andes_file = system.files.name + '.xlsx'
 
-    df_models = system.as_dict()
-
-    na_models = []
-    # Initialize an ANDES system
-    sa = andes_system(**kwargs)
-    sa.files.ext = '.xlsx'  # pseudo xlsx extension
-    for name, df in df_models.items():
-        # convert dict to dataframe
-        df = pd.DataFrame(df).drop(['uid'], axis=1)
-        try:
-            andes_mdl = getattr(sa, name)
-        except AttributeError:
-            na_models.append(name)
-            continue
-        andes_params = list(andes_mdl.params.keys())
-        columns_to_drop = list(set(df.columns) - set(andes_params))
-        df = pd.DataFrame(df).drop(columns_to_drop,
-                                   axis=1)
-        # drop rows that all nan
-        df.dropna(axis=0, how='all', inplace=True)
-        for row in df.to_dict(orient='records'):
-            sa.add(name, row)
+    xlsx.write(system, andes_file,
+               overwrite=overwrite,
+               to_andes=True,
+               **kwargs,
+               )
 
     _, s = elapsed(t0)
-    logger.info('System convert to ANDES in %s.', s)
+    logger.info(f'System convert to ANDES in {s}, save to "{andes_file}".')
 
-    if na_models:
-        logger.debug(f'No corresponding model found in ANDES: {na_models}')
+    sa = andes_load(andes_file, setup=setup, addfile=addfile, **kwargs)
 
     # Try parsing the addfile
     t, _ = elapsed()
 
     # additonal file for dynamic simulation
-    if addfile:
-        sa.files.addfile = addfile
-        sa.files.add_format = guess(sa)
-        logger.info('Parsing additional file "%s"...', sa.files.addfile)
-        add_parser = importlib.import_module('andes.io.' + sa.files.add_format)
-        try:
-            add_parser.read_add(sa, sa.files.addfile)
-            # logger.error('Error parsing addfile "%s" with %s parser.', sa.files.addfile, input_format)
-        except AttributeError:
-            add_parser.read(sa, addfile)
+    # if addfile:
+    #     sa.files.addfile = addfile
+    #     sa.files.add_format = guess(sa)
+    #     logger.info('Parsing additional file "%s"...', sa.files.addfile)
+    #     add_parser = importlib.import_module('andes.io.' + sa.files.add_format)
+    #     try:
+    #         add_parser.read_add(sa, sa.files.addfile)
+    #         # logger.error('Error parsing addfile "%s" with %s parser.', sa.files.addfile, input_format)
+    #     except AttributeError:
+    #         add_parser.read(sa, addfile)
 
-        # check SynGen Vn
-        syngen_idx = sa.SynGen.find_idx(keys='bus', values=sa.Bus.idx.v, allow_none=True, default=None)
-        syngen_idx = [x for x in syngen_idx if x is not None]
-        syngen_bus = sa.SynGen.get(src='bus', idx=syngen_idx, attr='v')
+    #     # check SynGen Vn
+    #     syngen_idx = sa.SynGen.find_idx(keys='bus', values=sa.Bus.idx.v, allow_none=True, default=None)
+    #     syngen_idx = [x for x in syngen_idx if x is not None]
+    #     syngen_bus = sa.SynGen.get(src='bus', idx=syngen_idx, attr='v')
 
-        gen_vn = sa.SynGen.get(src='Vn', idx=syngen_idx, attr='v')
-        bus_vn = sa.Bus.get(src='Vn', idx=syngen_bus, attr='v')
-        if not np.equal(gen_vn, bus_vn).all():
-            sa.SynGen.set(src='Vn', idx=syngen_idx, attr='v', value=bus_vn)
-            logger.warning('Correct SynGen Vn to match bus Vn.')
-        _, s = elapsed(t)
-        logger.info('Addfile parsed in %s.', s)
-
-    if setup:
-        sa.setup()
+    #     gen_vn = sa.SynGen.get(src='Vn', idx=syngen_idx, attr='v')
+    #     bus_vn = sa.Bus.get(src='Vn', idx=syngen_bus, attr='v')
+    #     if not np.equal(gen_vn, bus_vn).all():
+    #         sa.SynGen.set(src='Vn', idx=syngen_idx, attr='v', value=bus_vn)
+    #         logger.warning('Correct SynGen Vn to match bus Vn.')
+    #     _, s = elapsed(t)
+    #     logger.info('Addfile parsed in %s.', s)
 
     return sa
--- ams/io/xlsx.py
+++ ams/io/xlsx.py
@@ -3,5 +3,85 @@ Excel reader and writer for AMS.
 
 This module leverages the existing parser and writer in andes.io.xlsx.
 """
+import logging
 
-from andes.io.xlsx import (write, read, testlines)  # NOQA
+from andes.io.xlsx import (read, testlines, confirm_overwrite, _add_book)  # NOQA
+from andes.shared import pd
+from andes.system import System as andes_system
+
+
+logger = logging.getLogger(__name__)
+
+
+def write(system, outfile,
+          skip_empty=True, overwrite=None, add_book=None,
+          to_andes=False,
+          ):
+    """
+    Write loaded AMS system data into an xlsx file
+
+    Revised function ``andes.io.xlsx.write`` to skip non-andes models.
+
+    Parameters
+    ----------
+    system : System
+        A loaded system with parameters
+    outfile : str
+        Path to the output file
+    skip_empty : bool
+        Skip output of empty models (n = 0)
+    overwrite : bool, optional
+        None to prompt for overwrite selection; True to overwrite; False to not overwrite
+    add_book : str, optional
+        An optional model to be added to the output spreadsheet
+
+    Returns
+    -------
+    bool
+        True if file written; False otherwise
+    """
+    if not confirm_overwrite(outfile, overwrite=overwrite):
+        return False
+
+    writer = pd.ExcelWriter(outfile, engine='xlsxwriter')
+    writer = _write_system(system, writer, skip_empty, to_andes=to_andes)
+    writer = _add_book(system, writer, add_book)
+
+    writer.close()
+
+    logger.info('xlsx file written to "%s"', outfile)
+    return True
+
+
+def _write_system(system, writer, skip_empty, to_andes=True):
+    """
+    Write the system to pandas ExcelWriter
+
+    Rewrite function ``andes.io.xlsx._write_system`` to skip non-andes sheets.
+    """
+    na_models = []
+    skip_params = []
+    if to_andes:
+        # Initialize an ANDES system
+        sa = andes_system(setup=False, default_config=True,
+                          codegen=False, autogen_stale=False,
+                          no_undill=True,
+                          )
+    for name, instance in system.models.items():
+        if skip_empty and instance.n == 0:
+            continue
+        if to_andes:
+            if name not in sa.models.keys():
+                na_models.append(name)
+                continue
+        instance.cache.refresh("df_in")
+        if to_andes:
+            ams_params = list(instance.params.keys())
+            andes_params = list(sa.models[name].params.keys())
+            skip_params = list(set(ams_params) - set(andes_params))
+        if skip_params:
+            df = instance.cache.df_in.drop(skip_params, axis=1)
+        else:
+            df = instance.cache.df_in
+        df.to_excel(writer, sheet_name=name, freeze_panes=(1, 0))
+    return writer
