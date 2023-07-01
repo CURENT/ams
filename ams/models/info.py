74fbc7ad4fa70ec3f77d153427dd0691e1b2f019
jinningwang <jinningwang@users.noreply.github.com>
2023-07-01 14:51:33 -0400

    Add case summary info

--- ams/models/info.py
+++ ams/models/info.py
@@ -16,6 +16,7 @@ class Summary(ModelData, Model):
     def __init__(self, system, config):
         ModelData.__init__(self, three_params=False)
 
+        self.field = DataParam(info='field name')
         self.comment = DataParam(info='information, comment, or anything')
         self.comment2 = DataParam(info='comment field 2')
         self.comment3 = DataParam(info='comment field 3')
