35046aa5071cf5cae51db866d219c1f001f92ec9
jinningwang <jinningwang@users.noreply.github.com>
2023-07-11 13:51:59 -0400

    Refactor static gen load models

--- ams/models/static/gen.py
+++ ams/models/static/gen.py
@@ -1,60 +1,11 @@
 from collections import OrderedDict  # NOQA
 
 from andes.core.param import NumParam, ExtParam
-from andes.models.static.pq import PQData  # NOQA
 from andes.models.static.pv import PVData  # NOQA
 from andes.models.static.slack import SlackData  # NOQA
 
 from ams.core.model import Model
 from ams.core.var import Algeb  # NOQA
-
-
-class PQ(PQData, Model):
-    """
-    PQ load model.
-
-    TODO: implement type conversion in config
-    """
-
-    def __init__(self, system, config):
-        PQData.__init__(self)
-        Model.__init__(self, system, config)
-        self.group = 'StaticLoad'
-        self.config.add(OrderedDict((('pq2z', 1),
-                                     ('p2p', 0.0),
-                                     ('p2i', 0.0),
-                                     ('p2z', 1.0),
-                                     ('q2q', 0.0),
-                                     ('q2i', 0.0),
-                                     ('q2z', 1.0),
-                                     )))
-        self.config.add_extra("_help",
-                              pq2z="pq2z conversion if out of voltage limits",
-                              p2p="P constant power percentage for TDS. Must have (p2p+p2i+p2z)=1",
-                              p2i="P constant current percentage",
-                              p2z="P constant impedance percentage",
-                              q2q="Q constant power percentage for TDS. Must have (q2q+q2i+q2z)=1",
-                              q2i="Q constant current percentage",
-                              q2z="Q constant impedance percentage",
-                              )
-        self.config.add_extra("_alt",
-                              pq2z="(0, 1)",
-                              p2p="float",
-                              p2i="float",
-                              p2z="float",
-                              q2q="float",
-                              q2i="float",
-                              q2z="float",
-                              )
-        self.config.add_extra("_tex",
-                              pq2z="z_{pq2z}",
-                              p2p=r"\gamma_{p2p}",
-                              p2i=r"\gamma_{p2i}",
-                              p2z=r"\gamma_{p2z}",
-                              q2q=r"\gamma_{q2q}",
-                              q2i=r"\gamma_{q2i}",
-                              q2z=r"\gamma_{q2z}",
-                              )
 
 
 class GenParam:
