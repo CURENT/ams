0922304d79c4ce42668a8c8708d8b01dd7445563
jinningwang <jinningwang@users.noreply.github.com>
2023-09-14 14:24:01 -0400

    Add param gammap and gammaq to model regcv1

--- ams/models/renewable/regcv1.py
+++ ams/models/renewable/regcv1.py
@@ -32,3 +32,12 @@ class REGCV1(ModelData, Model):
         self.gen = IdxParam(info="static generator index",
                             mandatory=True,
                             )
+
+        self.gammap = NumParam(default=1.0,
+                               info="P ratio of linked static gen",
+                               tex_name=r'\gamma_P'
+                               )
+        self.gammaq = NumParam(default=1.0,
+                               info="Q ratio of linked static gen",
+                               tex_name=r'\gamma_Q'
+                               )
