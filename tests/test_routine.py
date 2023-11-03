24b06f21c102e24af16f94c73db977be1f127ee1
jinningwang <jinninggm@gmail.com>
2023-11-03 16:25:45 -0400

    Add routine tests

--- tests/test_routine.py
+++ tests/test_routine.py
@@ -2,6 +2,23 @@ import unittest
 import numpy as np
 
 import ams
+import cvxpy as cp
+
+
+def require_MIP_solver(f):
+    """
+    Decorator for skipping tests that require MIP solver.
+    """
+    def wrapper(*args, **kwargs):
+        all_solvers = cp.installed_solvers()
+        mip_solvers = ['CBC', 'COPT', 'GLPK_MI', 'CPLEX', 'GUROBI',
+                       'MOSEK', 'SCIP', 'XPRESS', 'SCIPY']
+        if any(s in mip_solvers for s in all_solvers):
+            pass
+        else:
+            raise unittest.SkipTest("MIP solver is not available.")
+        return f(*args, **kwargs)
+    return wrapper
 
 
 class TestRoutineMethods(unittest.TestCase):
@@ -9,7 +26,7 @@ class TestRoutineMethods(unittest.TestCase):
     Test methods of Routine.
     """
     def setUp(self) -> None:
-        self.ss = ams.load(ams.get_case("ieee39/ieee39_uced.xlsx"),
+        self.ss = ams.load(ams.get_case("ieee39/ieee39_uced_esd1.xlsx"),
                            default_config=True,
                            no_output=True,
                            )
@@ -37,3 +54,37 @@ class TestRoutineMethods(unittest.TestCase):
         self.assertEqual(self.ss.DCOPF.exit_code, 0, "Exit code is not 0.")
         np.testing.assert_equal(self.ss.DCOPF.get('pg', 'PV_30', 'v'),
                                 self.ss.StaticGen.get('p', 'PV_30', 'v'))
+
+    def test_RTED(self):
+        """
+        Test `RTED.run()`.
+        """
+
+        self.ss.RTED.run(solver='OSQP')
+        self.assertEqual(self.ss.RTED.exit_code, 0, "Exit code is not 0.")
+
+    @require_MIP_solver
+    def test_RTED2(self):
+        """
+        Test `RTED2.run()`.
+        """
+
+        self.ss.RTED2.run()
+        self.assertEqual(self.ss.RTED2.exit_code, 0, "Exit code is not 0.")
+
+    def test_ED(self):
+        """
+        Test `ED.run()`.
+        """
+
+        self.ss.ED.run(solver='OSQP')
+        self.assertEqual(self.ss.ED.exit_code, 0, "Exit code is not 0.")
+
+    @require_MIP_solver
+    def test_ED2(self):
+        """
+        Test `ED2.run()`.
+        """
+
+        self.ss.ED2.run()
+        self.assertEqual(self.ss.ED2.exit_code, 0, "Exit code is not 0.")
