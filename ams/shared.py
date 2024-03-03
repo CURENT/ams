a11c438707175a43f909896490c976c8c8ae2942
jinningwang <jinningwang>
2024-03-03 14:05:11 -0500

    Remove igraph

--- ams/shared.py
+++ ams/shared.py
@@ -9,13 +9,9 @@ from datetime import datetime
 
 import cvxpy as cp
 
-from andes.utils.lazyimport import LazyImport
-
 
 logger = logging.getLogger(__name__)
 
-
-igraph = LazyImport("import igraph")
 
 # NOTE: copied from CVXPY documentation
 MIP_SOLVERS = ['CBC', 'COPT', 'GLPK_MI', 'CPLEX', 'GUROBI',
@@ -37,22 +33,6 @@ def require_MIP_solver(f):
     def wrapper(*args, **kwargs):
         if not any(s in MIP_SOLVERS for s in INSTALLED_SOLVERS):
             raise ModuleNotFoundError("No MIP solver is available.")
-        return f(*args, **kwargs)
-
-    return wrapper
-
-
-def require_igraph(f):
-    """
-    Decorator for functions that require igraph.
-    """
-
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        try:
-            getattr(igraph, "__version__")
-        except AttributeError:
-            logger.error("Package `igraph` is not installed.")
         return f(*args, **kwargs)
 
     return wrapper
