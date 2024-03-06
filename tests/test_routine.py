34208aad2f28814141379c6940820fe191a78034
jinningwang <jinningwang>
2024-03-06 16:36:34 -0500

    Add test on RTED.dc2ac

--- tests/test_routine.py
+++ tests/test_routine.py
@@ -58,3 +58,38 @@ class TestRoutineMethods(unittest.TestCase):
         """
 
         self.assertTrue(self.ss.DCOPF.init(), "DCOPF initialization failed!")
+
+
+class TestRTED(unittest.TestCase):
+    """
+    Test methods of `RTED`.
+    """
+
+    def setUp(self) -> None:
+        self.ss = ams.load(ams.get_case("5bus/pjm5bus_demo.xlsx"),
+                           setup=True,
+                           default_config=True,
+                           no_output=True,
+                           )
+        self.ss.RTED.run(solver='ECOS')
+
+    def test_dc2ac(self):
+        """
+        Test `RTED.init()` method.
+        """
+        self.ss.RTED.dc2ac()
+        self.assertTrue(self.ss.RTED.is_ac, "AC conversion failed!")
+
+        stg_idx = self.ss.StaticGen.get_idx()
+        pg_rted = self.ss.RTED.get(src='pg', attr='v', idx=stg_idx)
+        pg_acopf = self.ss.ACOPF.get(src='pg', attr='v', idx=stg_idx)
+        np.testing.assert_almost_equal(pg_rted, pg_acopf, decimal=3)
+
+        bus_idx = self.ss.Bus.get_idx()
+        v_rted = self.ss.RTED.get(src='vBus', attr='v', idx=bus_idx)
+        v_acopf = self.ss.ACOPF.get(src='vBus', attr='v', idx=bus_idx)
+        np.testing.assert_almost_equal(v_rted, v_acopf, decimal=3)
+
+        a_rted = self.ss.RTED.get(src='aBus', attr='v', idx=bus_idx)
+        a_acopf = self.ss.ACOPF.get(src='aBus', attr='v', idx=bus_idx)
+        np.testing.assert_almost_equal(a_rted, a_acopf, decimal=3)
