7b86d9ac13eb1c8aabf624682c0cf6d170b5e1dc
jinningwang <jinninggm@gmail.com>
2024-11-01 23:21:55 -0400

    [WIP] Refactor test_dctypes, refactor test_rtn_rted

--- tests/test_rtn_rted.py
+++ tests/test_rtn_rted.py
@@ -2,6 +2,7 @@ import unittest
 import numpy as np
 
 import ams
+from ams.shared import skip_unittest_without_MIP
 
 
 class TestRTED(unittest.TestCase):
@@ -99,3 +100,77 @@ class TestRTED(unittest.TestCase):
         a_rted = self.ss.RTED.get(src='aBus', attr='v', idx=bus_idx)
         a_acopf = self.ss.ACOPF.get(src='aBus', attr='v', idx=bus_idx)
         np.testing.assert_almost_equal(a_rted, a_acopf, decimal=3)
+
+
+class TestRTEDES(unittest.TestCase):
+    """
+    Test routine `RTEDES`.
+    """
+
+    def setUp(self) -> None:
+        self.ss = ams.load(ams.get_case("5bus/pjm5bus_uced_esd1.xlsx"),
+                           setup=True, default_config=True, no_output=True)
+
+    def test_init(self):
+        """
+        Test initialization.
+        """
+        self.ss.RTEDES.init()
+        self.assertTrue(self.ss.RTEDES.initialized, "RTEDES initialization failed!")
+
+    @skip_unittest_without_MIP
+    def test_trip_gen(self):
+        """
+        Test generator tripping.
+        """
+        stg = 'PV_1'
+        self.ss.StaticGen.set(src='u', attr='v', idx=stg, value=0)
+
+        self.ss.RTEDES.update()
+        self.ss.RTEDES.run(solver='SCIP')
+        self.assertTrue(self.ss.RTEDES.converged, "RTEDES did not converge under generator trip!")
+        self.assertAlmostEqual(self.ss.RTEDES.get(src='pg', attr='v', idx=stg),
+                               0, places=6,
+                               msg="Generator trip does not take effect!")
+
+        self.ss.StaticGen.alter(src='u', idx=stg, value=1)
+
+    @skip_unittest_without_MIP
+    def test_trip_line(self):
+        """
+        Test line tripping.
+        """
+        self.ss.Line.set(src='u', attr='v', idx=3, value=0)
+
+        self.ss.RTEDES.update()
+        self.ss.RTEDES.run(solver='SCIP')
+        self.assertTrue(self.ss.RTEDES.converged, "RTEDES did not converge under line trip!")
+        self.assertAlmostEqual(self.ss.RTEDES.get(src='plf', attr='v', idx=3),
+                               0, places=6,
+                               msg="Line trip does not take effect!")
+
+        self.ss.Line.alter(src='u', idx=3, value=1)
+
+    @skip_unittest_without_MIP
+    def test_set_load(self):
+        """
+        Test setting and tripping load.
+        """
+        self.ss.RTEDES.run(solver='SCIP')
+        pgs = self.ss.RTEDES.obj.v.sum()
+
+        # --- set load ---
+        self.ss.PQ.set(src='p0', attr='v', idx='PQ_1', value=1)
+        self.ss.RTEDES.update()
+
+        self.ss.RTEDES.run(solver='SCIP')
+        pgs_pqt = self.ss.RTEDES.obj.v.sum()
+        self.assertLess(pgs_pqt, pgs, "Load set does not take effect!")
+
+        # --- trip load ---
+        self.ss.PQ.set(src='u', attr='v', idx='PQ_2', value=0)
+        self.ss.RTEDES.update()
+
+        self.ss.RTEDES.run(solver='SCIP')
+        pgs_pqt2 = self.ss.RTEDES.obj.v.sum()
+        self.assertLess(pgs_pqt2, pgs_pqt, "Load trip does not take effect!")
