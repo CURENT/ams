21065d841f94d26c130fafd8357636b06b016686
jinningwang <jinninggm@gmail.com>
2024-11-14 12:45:32 -0500

    Minor fix

--- ams/__init__.py
+++ ams/__init__.py
@@ -1,12 +1,6 @@
 from . import _version
 __version__ = _version.get_versions()['version']
 
-from ams import io          # NOQA
-from ams import core        # NOQA
-from ams import models      # NOQA
-from ams import routines    # NOQA
-from ams import utils       # NOQA
-from ams import interface   # NOQA
 from ams import benchmarks  # NOQA
 
 from ams.main import config_logger, load, run  # NOQA
@@ -16,4 +10,4 @@ from ams.shared import ppc2df  # NOQA
 
 __author__ = 'Jining Wang'
 
-__all__ = ['system', 'ppc2df', 'System']
+__all__ = ['System', 'get_case', 'System']
