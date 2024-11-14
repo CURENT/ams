4cc2045933e3e8a13bfb09cf96563237c71f0d19
jinningwang <jinninggm@gmail.com>
2024-11-14 09:00:18 -0500

    Add import routines in package init

--- ams/__init__.py
+++ ams/__init__.py
@@ -1,10 +1,13 @@
 from . import _version
 __version__ = _version.get_versions()['version']
 
+from ams import routines  # NOQA
+from ams import benchmarks  # NOQA
+
 from ams.main import config_logger, load, run  # NOQA
 from ams.utils.paths import get_case  # NOQA
 from ams.shared import ppc2df  # NOQA
 
 __author__ = 'Jining Wang'
 
-__all__ = ['system']
+__all__ = ['main', 'system', 'cli']
