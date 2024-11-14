c0a2dc541d29506002ba98b24dfd4039801bccbf
jinningwang <jinninggm@gmail.com>
2024-11-14 17:40:40 -0500

    Add test of versioninfo

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,12 +1,15 @@
 import unittest
 
-import ams
+import ams.main
 
 
 class TestCLI(unittest.TestCase):
     def test_main_doc(self):
         ams.main.doc('Bus')
         ams.main.doc(list_supported=True)
+
+    def test_versioninfo(self):
+        ams.main.versioninfo()
 
     def test_misc(self):
         ams.main.misc(show_license=True)
