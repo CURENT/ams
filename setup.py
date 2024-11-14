41d4e9fd48cfa31ad2642e649971c111c55fce63
jinningwang <jinninggm@gmail.com>
2024-11-14 16:46:41 -0500

    Minimize setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-import versioneer
-
-setup(
-    name='ltbams',
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    packages=find_packages(where="."),
-    package_data={
-        'ams': [
-            # When adding files here, remember to update MANIFEST.in as well,
-            # or else they will not be included in the distribution on PyPI!
-            # 'path/to/data_file',
-        ]
-    },
-)
+setup()
