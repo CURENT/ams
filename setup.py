03a5209815b852c317baec7e397f5aa847144a54
jinningwang <jinninggm@gmail.com>
2024-11-14 17:00:54 -0500

    Minimize setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-import versioneer
-
-setup(
-    name='ltbams',
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    packages=find_packages(where="."),
-)
+setup()
