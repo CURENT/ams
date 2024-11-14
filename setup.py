9dcd290dc6532149fcd0b4f26fb5e9af4bf4bf36
jinningwang <jinninggm@gmail.com>
2024-11-14 16:34:57 -0500

    Fix setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 import versioneer
 
 setup(
+    name='ltbams',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     packages=find_packages(where="."),
