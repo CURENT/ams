1a082a53f274b78adc0756127197cf8484ec2ff6
jinningwang <jinninggm@gmail.com>
2024-11-14 08:49:43 -0500

    Add included packages in setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,12 @@ import versioneer
 
 setup(
     version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass()
+    cmdclass=versioneer.get_cmdclass(),
+    package_data={
+        'ams': [
+            # When adding files here, remember to update MANIFEST.in as well,
+            # or else they will not be included in the distribution on PyPI!
+            # 'path/to/data_file',
+        ]
+    },
 )
