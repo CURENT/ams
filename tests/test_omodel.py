390d530853db295d03eccef45a9117a46bd1e3c7
jinningwang <jinninggm@gmail.com>
2024-11-15 07:31:18 -0500

    Add more tests on OModel __repr__

--- tests/test_omodel.py
+++ tests/test_omodel.py
@@ -62,3 +62,58 @@ class TestOModel(unittest.TestCase):
         self.assertTrue(self.ss.DCOPF.om.initialized, "OModel should be initialized after initialization!")
         self.ss.DCOPF.update('ug')
         self.assertTrue(self.ss.DCOPF.om.initialized, "OModel should be initialized after nonparametric update!")
+
+
+class TestOModelrepr(unittest.TestCase):
+    """
+    Test __repr__ in module `omodel`.
+    """
+
+    def setUp(self) -> None:
+        self.ss = ams.load(ams.get_case("5bus/pjm5bus_demo.xlsx"),
+                           setup=True,
+                           default_config=True,
+                           no_output=True,
+                           )
+
+    def test_dcopf(self):
+        """
+        Test `DCOPF.om` __repr__().
+        """
+        self.ss.DCOPF.om.parse()
+        self.assertIsInstance(self.ss.DCOPF.pg.__repr__(), str)
+        self.assertIsInstance(self.ss.DCOPF.pmax.__repr__(), str)
+        self.assertIsInstance(self.ss.DCOPF.pb.__repr__(), str)
+        self.assertIsInstance(self.ss.DCOPF.plflb.__repr__(), str)
+        self.assertIsInstance(self.ss.DCOPF.obj.__repr__(), str)
+
+    def test_rted(self):
+        """
+        Test `RTED.om` __repr__().
+        """
+        self.ss.RTED.om.parse()
+        self.assertIsInstance(self.ss.RTED.pru.__repr__(), str)
+        self.assertIsInstance(self.ss.RTED.R10.__repr__(), str)
+        self.assertIsInstance(self.ss.RTED.pru.__repr__(), str)
+        self.assertIsInstance(self.ss.RTED.rru.__repr__(), str)
+        self.assertIsInstance(self.ss.RTED.obj.__repr__(), str)
+
+    def test_ed(self):
+        """
+        Test `ED.om` __repr__().
+        """
+        self.ss.ED.om.parse()
+        self.assertIsInstance(self.ss.ED.prs.__repr__(), str)
+        self.assertIsInstance(self.ss.ED.R30.__repr__(), str)
+        self.assertIsInstance(self.ss.ED.prsb.__repr__(), str)
+        self.assertIsInstance(self.ss.ED.obj.__repr__(), str)
+
+    def test_uc(self):
+        """
+        Test `UC.om` __repr__().
+        """
+        self.ss.UC.om.parse()
+        self.assertIsInstance(self.ss.UC.prns.__repr__(), str)
+        self.assertIsInstance(self.ss.UC.cdp.__repr__(), str)
+        self.assertIsInstance(self.ss.UC.prnsb.__repr__(), str)
+        self.assertIsInstance(self.ss.UC.obj.__repr__(), str)
