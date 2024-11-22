5bf77f9b1cf6157b53232a47cbb917c729895882
jinningwang <jinninggm@gmail.com>
2024-11-22 07:44:29 -0500

    PFlow2, interface ANDES PFlow routine

--- ams/routines/pflow2.py
+++ ams/routines/pflow2.py
@@ -7,6 +7,7 @@ from collections import OrderedDict
 from andes.utils.misc import elapsed
 
 from ams.routines.routine import RoutineBase
+from ams.opt import Var
 from ams.interface import _to_andes_pflow
 
 logger = logging.getLogger(__name__)
@@ -16,12 +17,16 @@ class PFlow2(RoutineBase):
     """
     Power flow analysis using ANDES PFlow routine.
 
-    More PFlow settings can be changed via `PFlow2._adsys.config` and `PFlow2.om.config`.
+    More settings can be changed via `PFlow2._adsys.config` and `PFlow2._adsys.PFlow.config`.
+
+    All generator output powers, bus voltages, and angles are included in the variable definitions.
+    However, not all of these are unknowns; the definitions are provided for easy access.
 
     Reference
     ---------
 
-    [1] ANDES Documentation - Simulation and Plot, [Online],
+    [1] M. L. Crow, Computational methods for electric power systems. 2015.
+    [2] ANDES Documentation - Simulation and Plot, [Online],
 
     Available:
 
@@ -55,6 +60,23 @@ class PFlow2(RoutineBase):
                               n_factorize=">0",
                               )
 
+        self.pg = Var(info='Gen active power',
+                      unit='p.u.',
+                      name='pg', tex_name=r'p_g',
+                      model='StaticGen', src='p')
+        self.qg = Var(info='Gen reactive power',
+                      unit='p.u.',
+                      name='qg', tex_name=r'q_g',
+                      model='StaticGen', src='q')
+        self.aBus = Var(info='Bus voltage angle',
+                        unit='rad',
+                        name='aBus', tex_name=r'\theta_{bus}',
+                        model='Bus', src='a',)
+        self.vBus = Var(info='Bus voltage magnitude',
+                        unit='p.u.',
+                        name='vBus', tex_name=r'V_{bus}',
+                        model='Bus', src='v',)
+
     def init(self, **kwargs):
         """
         Initialize the ANDES PFlow routine.
@@ -66,7 +88,7 @@ class PFlow2(RoutineBase):
                                       config=self.config.as_dict(),
                                       **kwargs)
         self._adsys.setup()
-        self.om = self._adsys.PFlow
+        self.om.init()
         self.initialized = True
         return self.initialized
 
@@ -82,7 +104,7 @@ class PFlow2(RoutineBase):
         """
         # --- solve optimization ---
         t0, _ = elapsed()
-        _ = self.om.run()
+        _ = self._adsys.PFlow.run()
         self.exit_code = self._adsys.exit_code
         self.converged = self.exit_code == 0
         _, s = elapsed(t0)
@@ -101,21 +123,28 @@ class PFlow2(RoutineBase):
         Unpack the results from ANDES PFlow routine.
         """
         # TODO: maybe also include the DC devices results
-        bus_idx = self.system.Bus.idx.v
-        self.system.Bus.set(src='v', attr='v', idx=bus_idx,
-                            value=self._adsys.Bus.get(src='v', attr='v', idx=bus_idx))
-        self.system.Bus.set(src='a', attr='v', idx=bus_idx,
-                            value=self._adsys.Bus.get(src='a', attr='v', idx=bus_idx))
-        pv_idx = self.system.PV.idx.v
-        self.system.PV.set(src='p', attr='v', idx=pv_idx,
-                           value=self.system.PV.get(src='p0', attr='v', idx=pv_idx))
-        self.system.PV.set(src='q', attr='v', idx=pv_idx,
-                           value=self._adsys.PV.get(src='q', attr='v', idx=pv_idx))
-        slack_idx = self.system.Slack.idx.v
-        self.system.Slack.set(src='p', attr='v', idx=slack_idx,
-                              value=self._adsys.Slack.get(src='p', attr='v', idx=slack_idx))
-        self.system.Slack.set(src='q', attr='v', idx=slack_idx,
-                              value=self._adsys.Slack.get(src='q', attr='v', idx=slack_idx))
+        sys = self.system
+        # --- device results ---
+        bus_idx = sys.Bus.idx.v
+        sys.Bus.set(src='v', attr='v', idx=bus_idx,
+                    value=self._adsys.Bus.get(src='v', attr='v', idx=bus_idx))
+        sys.Bus.set(src='a', attr='v', idx=bus_idx,
+                    value=self._adsys.Bus.get(src='a', attr='v', idx=bus_idx))
+        pv_idx = sys.PV.idx.v
+        sys.PV.set(src='p', attr='v', idx=pv_idx,
+                   value=sys.PV.get(src='p0', attr='v', idx=pv_idx))
+        sys.PV.set(src='q', attr='v', idx=pv_idx,
+                   value=self._adsys.PV.get(src='q', attr='v', idx=pv_idx))
+        slack_idx = sys.Slack.idx.v
+        sys.Slack.set(src='p', attr='v', idx=slack_idx,
+                      value=self._adsys.Slack.get(src='p', attr='v', idx=slack_idx))
+        sys.Slack.set(src='q', attr='v', idx=slack_idx,
+                      value=self._adsys.Slack.get(src='q', attr='v', idx=slack_idx))
+        # --- routine results ---
+        self.pg.optz.value = sys.StaticGen.get(src='p', attr='v', idx=self.pg.get_idx())
+        self.qg.optz.value = sys.StaticGen.get(src='q', attr='v', idx=self.qg.get_idx())
+        self.aBus.optz.value = sys.Bus.get(src='a', attr='v', idx=self.aBus.get_idx())
+        self.vBus.optz.value = sys.Bus.get(src='v', attr='v', idx=self.vBus.get_idx())
         return True
 
     def update(self, **kwargs):
@@ -140,8 +169,4 @@ class PFlow2(RoutineBase):
         raise NotImplementedError
 
     def addVars(self, **kwargs):
-        raise NotImplementedError
-
-    def export_csv(self, path=None):
-        # TODO: customize the CSV output
         raise NotImplementedError
