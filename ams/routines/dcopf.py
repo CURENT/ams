4019458da679c07df90e177e9da40217292ca376
jinningwang <jinninggm@gmail.com>
2025-04-09 23:09:05 -0400

    Format

--- ams/routines/dcopf.py
+++ ams/routines/dcopf.py
@@ -27,9 +27,9 @@ class DCOPF(DCPFBase):
 
     References
     ----------
-    1. R. D. Zimmerman, C. E. Murillo-Sanchez, and R. J. Thomas, “MATPOWER: Steady-State Operations, Planning, and
-       Analysis Tools for Power Systems Research and Education,” IEEE Trans. Power Syst., vol. 26, no. 1, pp. 12-19,
-       Feb. 2011
+    1. R. D. Zimmerman, C. E. Murillo-Sanchez, and R. J. Thomas, “MATPOWER: Steady-State
+       Operations, Planning, and Analysis Tools for Power Systems Research and Education,” IEEE
+       Trans. Power Syst., vol. 26, no. 1, pp. 12-19, Feb. 2011
     """
 
     def __init__(self, system, config):
