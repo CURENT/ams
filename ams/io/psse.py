bd237cc46c6715ac4f8f828e7bc28f3c315538d6
jinningwang <jinninggm@gmail.com>
2025-05-19 22:41:28 -0400

    [WIP] PSSE raw writer, try generator section

--- ams/io/psse.py
+++ ams/io/psse.py
@@ -5,6 +5,11 @@ This module is the existing module in ``andes.io.psse``.
 
 from andes.io.psse import testlines  # NOQA
 from andes.io.psse import read as ad_read
+from andes.io.xlsx import confirm_overwrite
+from andes.shared import rad2deg
+
+from ams import __version__ as version
+from ams.shared import copyright_msg, nowarranty_msg, report_time
 
 
 def read(system, file):
@@ -35,3 +40,111 @@ def read(system, file):
         # Update the zone values in the system
         system.Bus.zone.v = [zone_map[z] for z in zone]
     return ret
+
+
+def write_raw(system, outfile: str, overwrite: bool = None):
+    """
+    Convert AMS system to PSS/E RAW file.
+
+    Parameters
+    ----------
+    system : System
+        The AMS system to be converted.
+    outfile : str
+        The output file path.
+    overwrite : bool, optional
+        If True, overwrite the file if it exists. If False, do not overwrite.
+    """
+    if not confirm_overwrite(outfile, overwrite=overwrite):
+        return False
+
+    mva = system.config.mva
+    freq = system.config.freq
+    name = system.files.name
+
+    with open(outfile, 'w') as f:
+        # PSS/E version and date
+        f.write(f"0, {mva:.2f}, 33, 0, 1, {freq:.2f}     ")
+
+        f.write(f"/ PSS/E 33 RAW, {report_time}\n")
+        f.write(f"Created by AMS {version}\n")
+        f.write(f"{copyright_msg}\n")
+        f.write(f"{nowarranty_msg}\n")
+        f.write(f"{name.upper()} \n")
+
+        # --- Bus ---
+        bus = system.Bus.cache.df_in
+        # 0,  1,    2,      3,    4,    5,    6,     7,  8,  9,    10
+        # ID, Name, BaseKV, Type, Area, Zone, Owner, Vm, Va, Vmax, Vmin
+        # Define column widths for alignment
+        column_widths = [6, 8, 8, 2, 3, 3, 3, 8, 8, 8, 8]
+        for row in bus.itertuples():
+            # Prepare the data for each column
+            data = [
+                int(row.idx), row.name, row.Vn, int(row.type),
+                int(system.Collection.idx2uid(row.area) + 1),
+                int(system.Collection.idx2uid(row.zone) + 1),
+                int(row.owner),
+                float(row.v0), float(row.a0 * rad2deg),
+                float(row.vmax), float(row.vmin)]
+            # Format each column with ',' as the delimiter
+            formatted_row = ",".join(
+                f"{value:>{width}}" if isinstance(value, (int, float)) else f"'{value:>{width - 2}}'"
+                for value, width in zip(data, column_widths)
+            ) + "\n"
+            # Write the formatted row to the file
+            f.write(formatted_row)
+
+        # --- Load ---
+        f.write("0 / END OF BUS DATA, BEGIN LOAD DATA\n")
+        load = system.PQ.cache.df_in
+        # 0,   1,  2,      3,    4,    5,      6,       7,  8,  9,  10, 11
+        # Bus, Id, Status, Area, Zone, PL(MW), QL (MW), IP, IQ, YP, YQ, Owner
+        # NOTE: load are converted to constant load, IP, IQ, YP, YQ are ignored by setting to 0
+        column_widths = [6, 8, 2, 3, 3, 10, 10, 2, 2, 10, 10, 3]
+        for row in load.itertuples():
+            # Prepare the data for each column
+            data = [
+                int(row.bus),
+                int(system.PQ.idx2uid(row.idx) + 1),
+                int(row.u),
+                int(system.Collection.idx2uid(system.PQ.get('area', row.idx, 'v')) + 1),
+                int(system.Collection.idx2uid(system.PQ.get('zone', row.idx, 'v')) + 1),
+                float(row.p0 * mva), float(row.q0 * mva),
+                float(0), float(0), float(0), float(0),
+                int(row.owner)]
+            # Format each column with ',' as the delimiter
+            formatted_row = ",".join(
+                f"{value:>{width}}" if isinstance(value, (int, float)) else f"'{value:>{width - 2}}'"
+                for value, width in zip(data, column_widths)
+            ) + "\n"
+            # Write the formatted row to the file
+            f.write(formatted_row)
+
+        # --- Fixed Shunt ---
+        f.write("0 / END OF LOAD DATA, BEGIN FIXED SHUNT DATA\n")
+        shunt = system.Shunt.cache.df_in
+        # 0,   1,  2,      3,      4
+        # Bus, Id, Status, G (MW), B (Mvar)
+        # NOTE: ANDES parse v33 swshunt into fixed shunt
+        column_widths = [6, 8, 2, 10, 10]
+        for row in shunt.itertuples():
+            # Prepare the data for each column
+            data = [
+                int(row.bus),
+                int(system.Shunt.idx2uid(row.idx) + 1),
+                int(row.u),
+                float(row.g * mva), float(row.b * mva)]
+
+            # Format each column with ',' as the delimiter
+            formatted_row = ",".join(
+                f"{value:>{width}}" if isinstance(value, (int, float)) else f"'{value:>{width - 2}}'"
+                for value, width in zip(data, column_widths)
+            ) + "\n"
+            # Write the formatted row to the file
+            f.write(formatted_row)
+
+        # End of file
+        f.write("Q\n")
+
+    return True
