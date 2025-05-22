a86bbf037efe114e3175308b381569a97b9acdae
jinningwang <jinninggm@gmail.com>
2025-05-22 17:13:18 -0400

    Follow best practice

--- ams/interface.py
+++ ams/interface.py
@@ -273,8 +273,7 @@ def parse_addfile(adsys, amsys, addfile):
         for keys, dct in json_in.items():
             reads[keys] = pd.DataFrame(dct)
     else:
-        logger.error('Addfile format "%s" is not supported yet.', add_format,
-                     'only xlsx and json are supported.')
+        logger.error("Unsupported addfile format, only 'xlsx' and 'json' formats are supported.")
         return adsys
 
     pflow_mdl = list(pflow_dict.keys())
