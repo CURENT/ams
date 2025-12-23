5e6bfbcc0aebd8b70e677d91c2bfb490a31f8654
jinningwang <jinninggm@gmail.com>
2025-12-22 16:50:11 -0800

    Add tests for RTEDES charging duration constraints

--- tests/test_rtn_rted.py
+++ tests/test_rtn_rted.py
@@ -271,3 +271,52 @@ class TestRTEDES(unittest.TestCase):
         self.ss.RTEDES.run(solver='SCIP')
         pgs_pqt2 = self.ss.RTEDES.pg.v.sum()
         self.assertLess(pgs_pqt2, pgs_pqt, "Load trip does not take effect!")
+
+    @skip_unittest_without_MISOCP
+    def test_ch_decision(self):
+        """
+        Test charging/discharging decision for charging/discharging duration time.
+
+        Scenarios to validate:
+                    res     ucd     tdc     tdc0
+        0           True    1       1.0     0.5
+        1           False   0       1.0     0.5
+        2           True    1       1.0     0.0
+        3           True    0       1.0     0.0
+        4           True    1       0.5     1.0
+        5           True    0       0.5     1.0
+        """
+        self.ss.RTEDES.init()
+
+        self.ss.ESD1.set(src='tdc0', attr='v', idx='ESD1_1', value=0.5)
+        self.ss.ESD1.set(src='tdc', attr='v', idx='ESD1_1', value=1.0)
+        # scenario 1: initially charging, charging duration time not met, decision to charge
+        self.ss.RTEDES.ucd.optz.value = np.array([1])
+        self.assertTrue(self.ss.RTEDES.tcdr.e[0] <= 0,
+                        "RTEDES.tcdr should be respected in scenario 1!")
+        # scenario 2: initially charging, with charging duration time not met, decision to charge
+        self.ss.RTEDES.ucd.optz.value = np.array([0])
+        self.assertFalse(self.ss.RTEDES.tcdr.e[0] <= 0,
+                         "RTEDES.tcdr should be broken in scenario 2!")
+
+        self.ss.ESD1.set(src='tdc0', attr='v', idx='ESD1_1', value=0.0)
+        self.ss.ESD1.set(src='tdc', attr='v', idx='ESD1_1', value=1.0)
+        # scenario 3: initially not charging, decision to charge
+        self.ss.RTEDES.ucd.optz.value = np.array([1])
+        self.assertTrue(self.ss.RTEDES.tcdr.e[0] <= 0,
+                        "RTEDES.tcdr should be respected in scenario 3!")
+        # scenario 4: initially not charging, decision to charge
+        self.ss.RTEDES.ucd.optz.value = np.array([0])
+        self.assertTrue(self.ss.RTEDES.tcdr.e[0] <= 0,
+                        "RTEDES.tcdr should be respected in scenario 4!")
+
+        self.ss.ESD1.set(src='tdc0', attr='v', idx='ESD1_1', value=1.0)
+        self.ss.ESD1.set(src='tdc', attr='v', idx='ESD1_1', value=0.5)
+        # scenario 5: initially charging, charging duration time met, decision to charge
+        self.ss.RTEDES.ucd.optz.value = np.array([1])
+        self.assertTrue(self.ss.RTEDES.tcdr.e[0] <= 0,
+                        "RTEDES.tcdr should be respected in scenario 5!")
+        # scenario 6: initially charging, charging duration time met, decision to charge
+        self.ss.RTEDES.ucd.optz.value = np.array([0])
+        self.assertTrue(self.ss.RTEDES.tcdr.e[0] <= 0,
+                        "RTEDES.tcdr should be respected in scenario 6!")
